fb806cf9c1f4d18fe4cc20121dd4053945d60006
Anatoly <0711975@gmail.com>
2021-06-25 12:28:03 +0300

    Подключена библиотека Electron JS для запуска приложения в системе

---
+++ script.js
@@ -0,0 +1,28 @@
+const url = require('url').format({
+    rotocol: 'file',
+    slashes: true,
+    pathname: require('path').join(__dirname, 'index.html')
+});
+
+const {app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
+        width: 500,
+        height: 850
+    });
+
+    win.loadURL(url);
+    win.on('closed', function(){
+    win = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function(){
+    app.quit();
+});
+
+
